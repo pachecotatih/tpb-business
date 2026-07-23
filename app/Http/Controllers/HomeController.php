1793219c74e53decbc389b265a613c26f125a4e0
Tatiana Pacheco Barreto <tatianapacheco09@gmail.com>
2026-07-23 17:16:09 -0300

    fix: Correção busca de dados de fluxo de caixa na home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,12 +17,13 @@ class HomeController extends Controller
             if(!$user) {
                 return response()->json(['message' => 'Usuário não encontrado'], 404);
             }
-            $fluxo_caixa = FluxoCaixa::where('user_id', $user->id);
-            $saldo_hoje = $fluxo_caixa->whereDate('data_pagamento', Carbon::now()->format('Y-m-d'))->sum('valor');
-            $entradas_hoje = $fluxo_caixa->whereDate('data_pagamento', Carbon::now()->format('Y-m-d'))->where('tipo_movimentacao', 'entrada')->sum('valor');
-            $saidas_hoje = $fluxo_caixa->whereDate('created_at', Carbon::now()->format('Y-m-d'))->where('tipo_movimentacao', 'saida')->sum('valor');
+            $saldo_hoje = FluxoCaixa::where('user_id', $user->id)->whereDate('created_at', Carbon::now()->format('Y-m-d'))->get()->sum(function ($item) {
+                return $item->tipo_movimentacao == 'entrada' ? $item->valor : $item->valor * (-1);
+            });
+            $entradas_hoje = FluxoCaixa::where('user_id', $user->id)->whereDate('created_at', Carbon::now()->format('Y-m-d'))->where('tipo_movimentacao', 'entrada')->sum('valor');
+            $saidas_hoje = FluxoCaixa::where('user_id', $user->id)->whereDate('created_at', Carbon::now()->format('Y-m-d'))->where('tipo_movimentacao', 'saida')->sum('valor');
             $agendamentos_hoje = Agendamento::with(['cliente', 'servicos:uid,nome'])->whereDate('data_inicio', Carbon::now()->format('Y-m-d'))->where('status', '!=', 'concluido')->where('user_id', $user->id)->get();
-            return response()->json(['saldo_hoje' => $saldo_hoje, 'entradas_hoje' => $entradas_hoje, 'saidas_hoje' => $saidas_hoje, 'agendamentos_hoje' => $agendamentos_hoje]);
+            return response()->json(['saldo_hoje' => $saldo_hoje, 'entradas_hoje' => $entradas_hoje, 'saidas_hoje' => $saidas_hoje * (-1), 'agendamentos_hoje' => $agendamentos_hoje]);
         } catch (\Throwable $th) {
             Log::error('HomeController::index - ' . $th->getMessage(). ' - ' . $th->getCode(). ' - ' . $th->getFile(). ' - ' . $th->getLine());
             return response()->json(['message' => 'Erro ao buscar serviços: ' . $th->getMessage()], 500);
